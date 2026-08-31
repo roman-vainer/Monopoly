e44f69e26e79c0e84bfb8d38bc7fed4275bc2cf8
Mooncake-sys <schwagerei.katharina@gmail.com>
2026-08-31 14:27:35 +0200

    Hinzugefügt: Methoden und switchcase mit Rückgabe per Console in Event.cs und EventSpace.cs. Vorbereitung des IUserInterfaces für generelle Display-Messages.

---
+++ Monopoly/UserInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopoly
+{
+    internal class UserInterface:IUserInterface
+    {
+        public void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
