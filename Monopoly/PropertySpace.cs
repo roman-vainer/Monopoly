a5ee795c7944f411279c0d2fb7f1e544baac94e2
Rom <7019267@gmail.com>
2026-08-31 09:25:57 +0200

    Aufbau der Structur

---
+++ Monopoly/PropertySpace.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopoly;
+
+public class PropertySpace : Space {
+    public override void ExecuteAction(Player player) {
+    }
+}
