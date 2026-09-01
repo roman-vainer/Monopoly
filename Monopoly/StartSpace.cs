7c7c12ab3aa04d547eb6db0daf2d509ce2eb6b97
bbk-223 <bbkddd@gmail.com>
2026-09-01 15:13:57 +0200

    StartSpace

--- Monopoly/StartSpace.cs
+++ Monopoly/StartSpace.cs
@@ -5,8 +5,39 @@ using System.Text;
 namespace Monopoly;
 
 public class StartSpace : Space {
+
+    public int Round { get; set; } = 0;
+
+    public int BoardSize { get; set; }
+
+
     public override string ExecuteAction(Player player) {
-        return "";
+        Random random = new Random();
+
+        if (random.Next(50) == 0 && Round >= 2)
+        {
+            player.IstAktiv = false;
+            return $"{player.Name} wurde getötet";
+        }
+
+
+            switch (Round)
+        {
+            case 1:
+                player.MoneyChanges(200);
+                Round++;
+                return $"{player.Name} hat die Startposition erreicht und erhält 200 Punkte";
+            case 2:
+                player.MoneyChanges(400);
+                Round++;
+                return $"{player.Name} hat die Startposition erreicht und erhält 400 Punkte";
+            case 3:
+                player.MoneyChanges(600);
+                Round++;
+                return $"{player.Name} hat die Startposition erreicht und erhält 600 Punkte";
+            case >= 4:
+                return $"{player.Name} hat die Spiel erreicht";
+        }
 
     }
 }
