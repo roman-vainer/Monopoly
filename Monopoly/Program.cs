afe4f03e6b57042bdf1499306fbdda0afe43c466
Rom <7019267@gmail.com>
2026-09-01 10:29:37 +0200

    test

--- Monopoly/Program.cs
+++ Monopoly/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace Monopoly {
     internal class Program {
         static void Main(string[] args) {
-            int size = 20;
+            int size = 28;
             List<Player> players = CreatePlayers();
             Game game = new Game(players, size);
             game.Start();
