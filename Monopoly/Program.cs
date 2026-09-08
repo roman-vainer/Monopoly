0f082ab68c4de59f9bf6766c4a338057c6f620ce
Rom <7019267@gmail.com>
2026-09-08 15:36:17 +0200

    Neue UML

--- Monopoly/Program.cs
+++ Monopoly/Program.cs
@@ -5,16 +5,17 @@ internal class Program
     static void Main(string[] args)
     {
         int size = 28;
+        int playerCount = 4; ;
         Player.BoardSize = size;
-        List<Player> players = CreatePlayers();
+        List<Player> players = CreatePlayers(playerCount);
         Game game = new Game(players, size);
         game.Start();
     }
 
-    private static List<Player> CreatePlayers()
+    private static List<Player> CreatePlayers(int playerCount)
     {
         var players = new List<Player>();
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < playerCount; i++)
         {
             Console.WriteLine($"Geben Sie den Namen der {i + 1}. Spielers ein");
             players.Add(new Player(Console.ReadLine()!));
